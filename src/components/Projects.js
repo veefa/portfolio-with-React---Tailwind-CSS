0ef2b115129f731978b802a0a9c9ad9b53b68579
vefa <44091688+veefa@users.noreply.github.com>
2024-03-11 20:50:28 +0000

    .

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,38 +1,29 @@
-
-
 import React from "react";
 import choredoor from "../media/chore-door.png";
 import rockpaperscissores from "../media/r-p-s.png";
 import space from "../media/space.png";
 
-
-
-
-
 const Projects = () => {
-    const projects = [
-        {
-          img: choredoor,
-          title: "chore-door",
-          desc: " A multi author blog. Built with Node.js, MongoDB, React, Redux and Tailwind CSS ",
-          live: "https://amaizingspace.netlify.app/",
-          code: "https://github.com/Coderamrin/devlog",
-        },
-        {
-            img: space,
-            title: "space",
-            desc: "Free website template directory for SaaS and Degital Agency. Built with Bootstrap, JQuery and JavaScript",
-            live: "https://doorechore.netlify.app/",
-            code: "https://github.com/Coderamrin/html-templates",
-          },
-          {
-            img: rockpaperscissores,
-            title: "rockpaperscissores",
-            desc: "Free website template directory for SaaS and Degital Agency. Built with Bootstrap, JQuery and JavaScript",
-            live: "https://doorechore.netlify.app/",
-            code: "https://github.com/Coderamrin/html-templates",
-          }
-        ];
+  const projects = [
+    {
+      img: choredoor,
+      title: "chore-door",
+      live: "https://amaizingspace.netlify.app/",
+      code: "https://github.com/Coderamrin/devlog",
+    },
+    {
+      img: space,
+      title: "space",
+      live: "https://doorechore.netlify.app/",
+      code: "https://github.com/Coderamrin/html-templates",
+    },
+    {
+      img: rockpaperscissores,
+      title: "rockpaperscissores",
+      live: "https://doorechore.netlify.app/",
+      code: "https://github.com/Coderamrin/html-templates",
+    },
+  ];
 
   return (
     <section className="bg-primary text-white px-5 py-32" id="projects">
@@ -43,7 +34,8 @@ const Projects = () => {
           </h2>
 
           <p className="pb-5">
-            These are some of my projects. I have built these with React,Javascript and vanilla CSS. Check them out.
+            These are some of my projects. I have built these with
+            React,Javascript and vanilla CSS. Check them out.
           </p>
         </div>
 
@@ -54,13 +46,9 @@ const Projects = () => {
         {projects.map((project, i) => {
           return (
             <div className="relative" key={i}>
-              <img  src={project.img} alt={project.title} />
-              <div className="flex absolute left-0 right-0 top-[13px] bottom-0 mx-auto w-[90%] h-[90%]  bg-primary  opacity-0 duration-500 justify-center flex-col hover:opacity-100 ">
-                <p className="py-5 text-center font-bold px-2 text-white">
-                  {project.desc}
-                </p>
-
-                <div className="mx-auto">
+              <img src={project.img} alt={project.title} />
+              <div className="flex absolute left-0 right-3 top-[13px] bottom-0 mx-auto w-[70%] h-[70%]  bg-primary  opacity-0 duration-500 justify-center flex-col hover:opacity-100 ">
+              <div className="mx-auto">
                   <a
                     href={project.live}
                     className="px-5 py-2 bg-blue-500 hover:bg-blue-600 mr-5 font-bold"
